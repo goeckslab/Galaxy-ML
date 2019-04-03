81a4b9116da2483f9fc96e27d18786abbd7674f7
Qiang Gu <guqiang01@gmail.com>
2019-04-02 23:14:01 -0700

    make TDMScaler

---
+++ tests/test_tdmscaler.py
@@ -0,0 +1,19 @@
+import numpy as np
+import warnings
+from preprocessors import TDMScaler
+from nose.tools import raises
+
+warnings.simplefilter('ignore')
+
+
+X = [[ 1., -2.,  2.],
+    [ -2.,  1.,  3.],
+    [ 4.,  1., -2.]]
+
+
+def test_self_transform():
+    scaler = TDMScaler()
+    scaler.fit(X)
+    got = scaler.transform(X)
+
+    assert np.array_equal(X, got), got
