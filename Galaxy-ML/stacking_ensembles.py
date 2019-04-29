d9a4e87adbb4485c013c3c4ade0f91bb79efe417
Qiang Gu <guqiang01@gmail.com>
2019-04-28 23:02:34 -0700

    make explicit base estimator options for stacking ensembles

--- Galaxy-ML/stacking_ensembles.py
+++ Galaxy-ML/stacking_ensembles.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import os
 import pandas as pd
 import pickle
 import warnings
@@ -19,28 +20,30 @@ from iraps_classifier import (IRAPSCore, IRAPSClassifier,
                               BinarizeTargetClassifier,
                               BinarizeTargetRegressor)
 from preprocessors import Z_RandomOverSampler
-from utils import load_model, get_cv, get_search_params
+from utils import load_model, get_cv, get_estimator, get_search_params
 
 
 warnings.filterwarnings('ignore')
 
+N_JOBS = int(os.environ.get('GALAXY_SLOTS', 1))
 
-def main(base_paths, meta_path, inputs_path, output_obj,
+
+def main(inputs_path, output_obj, base_paths=None, meta_path=None,
          outfile_params=None):
     """
     Parameter
     ---------
-    base_paths : list of str(s)
-        List of estimator file paths
-
-    meta_path : str
-        File path
-
     inputs_path : str
         File path for Galaxy parameters
 
     output_obj : str
         File path for ensemble estimator ouput
+
+    base_paths : str
+        File path or paths concatenated by comma.
+
+    meta_path : str
+        File path
 
     outfile_params : str
         File path for params output
@@ -49,14 +52,22 @@ def main(base_paths, meta_path, inputs_path, output_obj,
         params = json.load(param_handler)
 
     base_estimators = []
-    for base_file in base_paths.split(','):
-        if base_file:
+    for idx, base_file in enumerate(base_paths.split(',')):
+        if base_file and base_file != 'None':
             with open(base_file, 'rb') as handler:
                 model = load_model(handler)
-                base_estimators.append(model)
+        else:
+            estimator_json = (params['base_est_builder'][idx]
+                              ['estimator_selector'])
+            model = get_estimator(estimator_json)
+        base_estimators.append(model)
 
-    with open(meta_path, 'rb') as f:
-        meta_estimator = load_model(f)
+    if meta_path:
+        with open(meta_path, 'rb') as f:
+            meta_estimator = load_model(f)
+    else:
+        estimator_json = params['meta_estimator']['estimator_selector']
+        meta_estimator = get_estimator(estimator_json)
 
     options = params['algo_selection']['options']
 
@@ -64,10 +75,8 @@ def main(base_paths, meta_path, inputs_path, output_obj,
     if cv_selector:
         splitter, groups = get_cv(cv_selector)
         options['cv'] = splitter
-
-    # set cv `shuffle` to False
-    if 'cv' in options:
-        options['shuffle'] = False
+        # set n_jobs
+        options['n_jobs'] = N_JOBS
 
     if params['algo_selection']['estimator_type'] == 'StackingCVClassifier':
         ensemble_estimator = StackingCVClassifier(
@@ -94,6 +103,9 @@ def main(base_paths, meta_path, inputs_path, output_obj,
             **options)
 
     print(ensemble_estimator)
+    for base_est in base_estimators:
+        print(base_est)
+
     with open(output_obj, 'wb') as out_handler:
         pickle.dump(ensemble_estimator, out_handler, pickle.HIGHEST_PROTOCOL)
 
@@ -112,5 +124,5 @@ if __name__ == '__main__':
     aparser.add_argument("-p", "--outfile_params", dest="outfile_params")
     args = aparser.parse_args()
 
-    main(args.bases, args.meta, args.inputs, args.outfile,
-         outfile_params=args.outfile_params)
+    main(args.inputs, args.outfile, base_paths=args.bases,
+         meta_path=args.meta, outfile_params=args.outfile_params)
