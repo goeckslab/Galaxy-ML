2a8da0cd78096ed67384e89cdee0ccfb835d9c7b
Qiang Gu <guqiang01@gmail.com>
2019-05-13 12:34:36 -0700

    remove exec import system and refactor safepicker

--- galaxy_ml/utils.py
+++ galaxy_ml/utils.py
@@ -2,7 +2,6 @@ import ast
 import json
 import imblearn
 import numpy as np
-import os
 import pandas
 import pickle
 import re
@@ -17,6 +16,7 @@ from collections import Counter
 from asteval import Interpreter, make_symbol_table
 from imblearn import under_sampling, over_sampling, combine
 from imblearn.pipeline import Pipeline as imbPipeline
+from mlxtend import regressor, classifier
 from scipy.io import mmread
 from sklearn import (
     cluster, compose, decomposition, ensemble, feature_extraction,
@@ -25,96 +25,45 @@ from sklearn import (
     svm, linear_model, tree, discriminant_analysis)
 
 try:
-    import mlxtend
-    from mlxtend import regressor, classifier
-except ImportError as e:
-    print(e)
+    import iraps_classifier
+except ImportError:
     pass
 
 try:
-    IRAPSClassifier
-except NameError:
-    try:
-        from iraps_classifier import (
-            IRAPSCore, IRAPSClassifier, BinarizeTargetClassifier,
-            BinarizeTargetRegressor, binarize_auc_scorer,
-            binarize_average_precision_scorer)
-    except ImportError as e:
-        print(e)
-        pass
+    import model_validations
+except ImportError:
+    pass
 
 try:
-    OrderedKFold
-except NameError:
-    try:
-        from model_validations import OrderedKFold, RepeatedOrderedKFold
-    except ImportError as e:
-        print(e)
-        pass
+    import feature_selectors
+except ImportError:
+    pass
 
 try:
-    DyRFECV
-except NameError:
-    # feature_selectors is required to use `utils`
-    try:
-        from feature_selectors import (DyRFE, DyRFECV,
-                                       check_feature_importances)
-    except ImportError as e:
-        print(e)
-        pass
-
-try:
-    Z_RandomOverSampler
-except NameError:
-    try:
-        from preprocessors import Z_RandomOverSampler
-    except ImportError as e:
-        print(e)
-        pass
+    import preprocessors
+except ImportError:
+    pass
 
 # handle pickle white list file
-try:
-    WL_FILE  # global white list file from tools
-except NameError:
-    WL_FILE = os.path.join(os.path.dirname(__file__), 'pk_whitelist.json')
+WL_FILE = __import__('os').path.join(
+    __import__('os').path.dirname(__file__), 'pk_whitelist.json')
 
-N_JOBS = int(os.environ.get('GALAXY_SLOTS', 1))
+N_JOBS = int(__import__('os').environ.get('GALAXY_SLOTS', 1))
 
 
-class SafePickler(pickle.Unpickler, object):
+class _SafePickler(pickle.Unpickler, object):
     """
     Used to safely deserialize scikit-learn model objects
     Usage:
-        eg.: SafePickler.load(pickled_file_object)
+        eg.: _SafePickler.load(pickled_file_object)
     """
     def __init__(self, file):
-        super(SafePickler, self).__init__(file)
+        super(_SafePickler, self).__init__(file)
         # load global white list
         with open(WL_FILE, 'r') as f:
             self.pk_whitelist = json.load(f)
 
-    def find_class(self, module, name):
-        """ override """
-        pk_whitelist = self.pk_whitelist
-
-        # custom estimators
-        if module in ['__main__', 'keras_galaxy_models',
-                      'feature_selectors', 'preprocessors',
-                      'iraps_classifier', 'model_validations']:
-            custom_classes = {
-                'IRAPSCore': IRAPSCore,
-                'IRAPSClassifier': IRAPSClassifier,
-                'BinarizeTargetClassifier': BinarizeTargetClassifier,
-                'BinarizeTargetRegressor': BinarizeTargetRegressor,
-                'OrderedKFold': OrderedKFold,
-                'RepeatedOrderedKFold': RepeatedOrderedKFold,
-                'Z_RandomOverSampler': Z_RandomOverSampler,
-                'DyRFECV': DyRFECV,
-                'DyRFE': DyRFE
-            }
-            return custom_classes[name]
-
-        bad_names = (
+        self.bad_names = (
             'and', 'as', 'assert', 'break', 'class', 'continue',
             'def', 'del', 'elif', 'else', 'except', 'exec',
             'finally', 'for', 'from', 'global', 'if', 'import',
@@ -129,18 +78,46 @@ class SafePickler(pickle.Unpickler, object):
             'im_class', 'im_func', 'im_self', 'gi_code', 'gi_frame',
             '__asteval__', 'f_locals', '__mro__')
 
-        good_names = ['copy_reg._reconstructor', '__builtin__.object',
-                      '__builtin__.bytearray', 'builtins.object',
-                      'builtins.bytearray']
+        # unclassified good globals
+        self.good_names = [
+            'copy_reg._reconstructor', '__builtin__.object',
+            '__builtin__.bytearray', 'builtins.object',
+            'builtins.bytearray', 'keras.engine.sequential.Sequential',
+            'keras.engine.sequential.Model']
 
+        # custom module in Galaxy-ML
+        self.custom_modules = [
+            '__main__', 'keras_galaxy_models', 'feature_selectors',
+            'preprocessors', 'iraps_classifier', 'model_validations']
+
+    # override
+    def find_class(self, module, name):
+        # balack list first
+        if name in self.bad_names:
+            raise pickle.UnpicklingError("global '%s.%s' is forbidden"
+                                         % (module, name))
+
+        # custom module in Galaxy-ML
+        if module in self.custom_modules:
+            cutom_module = sys.modules.get(module, None)
+            if cutom_module:
+                return getattr(cutom_module, name)
+            else:
+                raise pickle.UnpicklingError("Module %s' is not imported"
+                                             % module)
+
+        # For objects from outside libraries, it's necessary to verify
+        # both module and name. Currently only a blacklist checker
+        # is working.
+        # TODO: replace with a whitelist checker.
+        good_names = self.good_names
+        pk_whitelist = self.pk_whitelist
         if re.match(r'^[a-zA-Z_][a-zA-Z0-9_]*$', name):
             fullname = module + '.' + name
             if (fullname in good_names)\
-                or ((module.startswith(('sklearn.', 'xgboost.', 'skrebate.',
-                                        'imblearn.', 'mlxtend.', 'numpy.'))
-                     or module == 'numpy')
-                    and (name not in bad_names)):
-                # TODO: replace with a whitelist checker
+                or (module.startswith(('sklearn.', 'xgboost.', 'skrebate.',
+                                       'imblearn.', 'mlxtend.', 'numpy.'))
+                    or module == 'numpy'):
                 if fullname not in (pk_whitelist['SK_NAMES'] +
                                     pk_whitelist['SKR_NAMES'] +
                                     pk_whitelist['XGB_NAMES'] +
@@ -148,6 +125,7 @@ class SafePickler(pickle.Unpickler, object):
                                     pk_whitelist['IMBLEARN_NAMES'] +
                                     pk_whitelist['MLXTEND_NAMES'] +
                                     good_names):
+                    # raise pickle.UnpicklingError
                     print("Warning: global %s is not in pickler whitelist "
                           "yet and will loss support soon. Contact tool "
                           "author or leave a message at github.com" % fullname)
@@ -158,11 +136,15 @@ class SafePickler(pickle.Unpickler, object):
 
 
 def load_model(file):
-    return SafePickler(file).load()
+    """Load pickled object with `_SafePicker`
+    """
+    return _SafePickler(file).load()
 
 
 def read_columns(f, c=None, c_option='by_index_number',
                  return_df=False, **args):
+    """Return array from a tabular dataset by various columns selection
+    """
     data = pandas.read_csv(f, **args)
     if c_option == 'by_index_number':
         cols = list(map(lambda x: x - 1, c))
@@ -183,8 +165,18 @@ def read_columns(f, c=None, c_option='by_index_number',
         return y
 
 
-# generate an instance for one of sklearn.feature_selection classes
-def feature_selector(inputs):
+def feature_selector(inputs, X=None, y=None):
+    """generate an instance of sklearn.feature_selection classes
+
+    Parameters
+    ----------
+    inputs : dict
+        From galaxy tool parameters.
+    X : array
+        Containing training features.
+    y : array or list
+        Target values.
+    """
     selector = inputs['selected_algorithm']
     if selector != 'DyRFECV':
         selector = getattr(sklearn.feature_selection, selector)
@@ -206,6 +198,8 @@ def feature_selector(inputs):
         else:
             estimator_json = inputs['model_inputter']['estimator_selector']
             estimator = get_estimator(estimator_json)
+            check_feature_importances = try_get_attr(
+                'feature_selectors', 'check_feature_importances')
             estimator = check_feature_importances(estimator)
             new_selector = selector(estimator, **options)
 
@@ -214,6 +208,8 @@ def feature_selector(inputs):
         if step and step >= 1.0:
             options['step'] = int(step)
         estimator = get_estimator(inputs["estimator_selector"])
+        check_feature_importances = try_get_attr(
+            'feature_selectors', 'check_feature_importances')
         estimator = check_feature_importances(estimator)
         new_selector = selector(estimator, **options)
 
@@ -229,6 +225,8 @@ def feature_selector(inputs):
         if step and step >= 1.0:
             options['step'] = int(step)
         estimator = get_estimator(inputs['estimator_selector'])
+        check_feature_importances = try_get_attr(
+            'feature_selectors', 'check_feature_importances')
         estimator = check_feature_importances(estimator)
         new_selector = selector(estimator, **options)
 
@@ -247,7 +245,11 @@ def feature_selector(inputs):
             step = ast.literal_eval(step)
         options['step'] = step
         estimator = get_estimator(inputs["estimator_selector"])
+        check_feature_importances = try_get_attr(
+            'feature_selectors', 'check_feature_importances')
         estimator = check_feature_importances(estimator)
+        DyRFECV = try_get_attr('feature_selectors', 'DyRFECV')
+
         new_selector = DyRFECV(estimator, **options)
 
     elif inputs['selected_algorithm'] == 'VarianceThreshold':
@@ -262,6 +264,8 @@ def feature_selector(inputs):
 
 
 def get_X_y(params, file1, file2):
+    """Return machine learning inputs X, y from tabluar inputs
+    """
     input_type = (params['selected_tasks']['selected_algorithms']
                   ['input_options']['selected_input'])
     if input_type == 'tabular':
@@ -310,7 +314,8 @@ def get_X_y(params, file1, file2):
 
 
 class SafeEval(Interpreter):
-
+    """Customized symbol table for safely literal eval
+    """
     def __init__(self, load_scipy=False, load_numpy=False,
                  load_estimators=False):
 
@@ -390,7 +395,8 @@ class SafeEval(Interpreter):
 
 
 def get_estimator(estimator_json):
-
+    """Return a sklearn or compatible estimator from Galaxy tool inputs
+    """
     estimator_module = estimator_json['selected_module']
 
     if estimator_module == 'custom_estimator':
@@ -410,9 +416,12 @@ def get_estimator(estimator_json):
             options['value'] = estimator_json['value']
         options['less_is_positive'] = estimator_json['less_is_positive']
         if estimator_json['clf_or_regr'] == 'BinarizeTargetClassifier':
-            return BinarizeTargetClassifier(wrapped_estimator, **options)
+            klass = try_get_attr('iraps_classifier',
+                                 'BinarizeTargetClassifier')
         else:
-            return BinarizeTargetRegressor(wrapped_estimator, **options)
+            klass = try_get_attr('iraps_classifier',
+                                 'BinarizeTargetRegressor')
+        return klass(wrapped_estimator, **options)
 
     estimator_cls = estimator_json['selected_estimator']
 
@@ -439,8 +448,12 @@ def get_estimator(estimator_json):
 
 
 def get_cv(cv_json):
-    """
-    cv_json:
+    """ Return CV splitter from Galaxy tool inputs
+
+    Parameters
+    ----------
+    cv_json : dict
+        From Galaxy tool inputs.
         e.g.:
             {
                 'selected_cv': 'StratifiedKFold',
@@ -490,9 +503,9 @@ def get_cv(cv_json):
         cv_json['test_size'] = int(test_size)
 
     if cv == 'OrderedKFold':
-        cv_class = OrderedKFold
+        cv_class = try_get_attr('model_validations', 'OrderedKFold')
     elif cv == 'RepeatedOrderedKFold':
-        cv_class = RepeatedOrderedKFold
+        cv_class = try_get_attr('model_validations', 'RepeatedOrderedKFold')
     else:
         cv_class = getattr(model_selection, cv)
     splitter = cv_class(**cv_json)
@@ -502,6 +515,9 @@ def get_cv(cv_json):
 
 # needed when sklearn < v0.20
 def balanced_accuracy_score(y_true, y_pred):
+    """Compute balanced accuracy score, which is now available in
+        scikit-learn from v0.20.0.
+    """
     C = metrics.confusion_matrix(y_true, y_pred)
     with np.errstate(divide='ignore', invalid='ignore'):
         per_class = np.diag(C) / C.sum(axis=1)
@@ -513,14 +529,17 @@ def balanced_accuracy_score(y_true, y_pred):
 
 
 def get_scoring(scoring_json):
-
+    """Return single sklearn scorer class
+        or multiple scoers in dictionary
+    """
     if scoring_json['primary_scoring'] == 'default':
         return None
 
     my_scorers = metrics.SCORERS
-    my_scorers['binarize_auc_scorer'] = binarize_auc_scorer
+    my_scorers['binarize_auc_scorer'] =\
+        try_get_attr('iraps_classifier', 'binarize_auc_scorer')
     my_scorers['binarize_average_precision_scorer'] =\
-        binarize_average_precision_scorer
+        try_get_attr('iraps_classifier', 'binarize_average_precision_scorer')
     if 'balanced_accuracy' not in my_scorers:
         my_scorers['balanced_accuracy'] =\
             metrics.make_scorer(balanced_accuracy_score)
@@ -557,3 +576,24 @@ def get_search_params(estimator):
          "@, params eligible for search in searchcv tool."])
 
     return results
+
+
+def try_get_attr(module, name):
+    """try to get attribute from a custom module
+
+    Parameters
+    ----------
+    module : str
+        Module name
+    name : str
+        Attribute (class/function) name.
+
+    Returns
+    -------
+    class or function
+    """
+    mod = sys.modules.get(module, None)
+    if mod:
+        return getattr(mod, name)
+    else:
+        raise Exception("No module named %s." % module)
