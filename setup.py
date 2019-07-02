7d0cd86d251de54ba415b465cfac14a2e4ea3fcc
Qiang Gu <guqiang01@gmail.com>
2019-07-01 18:29:55 -0700

    push to version 0.7.3 and publish first pypi library

--- setup.py
+++ setup.py
@@ -4,23 +4,30 @@ try:
 except ImportError:
     subprocess.run("pip install Cython", shell=True, check=True)
 
-import numpy as np
+try:
+    import numpy as np
+except ImportError:
+    subprocess.run("pip install numpy>=1.16.2",
+                   shell=True, check=True)
+
 from os.path import realpath, dirname, join
 from setuptools import find_packages
 from distutils.core import setup
 from Cython.Distutils.extension import Extension
 from Cython.Distutils import build_ext
 from Cython.Build import cythonize
+import numpy as np
 import galaxy_ml
 
 
 VERSION = galaxy_ml.__version__
 PROJECT_ROOT = dirname(realpath(__file__))
 
-REQUIREMENTS_FILE = join(PROJECT_ROOT, 'requirements.txt')
-
-with open(REQUIREMENTS_FILE) as f:
+with open(join(PROJECT_ROOT, 'requirements.txt'), 'r') as f:
     install_reqs = f.read().splitlines()
+
+with open(join(PROJECT_ROOT, 'README.md'), 'r') as fh:
+    long_description = fh.read()
 
 genome_module = Extension(
     "galaxy_ml.externals.selene_sdk.sequences._sequence",
@@ -38,8 +45,10 @@ cmdclass = {'build_ext': build_ext}
 setup(name='Galaxy-ML',
       version=VERSION,
       description='Galaxy Machine Learning Library',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/goeckslab/Galaxy-ML/',
-      packages=find_packages(),
+      packages=find_packages(exclude=['tests*', 'test-data*']),
       package_data={
         '': ['README.md',
             'requirements.txt']
@@ -49,6 +58,11 @@ setup(name='Galaxy-ML',
       extras_require={'docs': ['mkdocs']},
       platforms='any',
       ext_modules=cythonize(ext_modules),
-      cmdclass=cmdclass
+      cmdclass=cmdclass,
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ]
 )
 
