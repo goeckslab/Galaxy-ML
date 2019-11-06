72ff6848510e7181884b10011be819ccd459c2ba
Qiang Gu <guqiang01@gmail.com>
2019-11-06 12:42:35 -0800

    add n_jobs handling in clean_params

--- galaxy_ml/utils.py
+++ galaxy_ml/utils.py
@@ -727,6 +727,11 @@ def get_module(module):
 
 
 def get_main_estimator(estimator):
+    """return main estimator. For pipeline, main estimator's
+    final estimator. For fitted SearchCV object, that's the
+    best_estimator_. For stacking ensemble family estimator,
+    main estimator is the meta estimator.
+    """
     est_name = estimator.__class__.__name__
     # support pipeline object
     if isinstance(estimator, Pipeline):
@@ -744,8 +749,10 @@ def get_main_estimator(estimator):
         return estimator
 
 
-def clean_params(estimator):
+def clean_params(estimator, n_jobs=None):
     """clean unwanted hyperparameter settings
+
+    If n_jobs is not None, set it into the estimator, if applicable
 
     Return
     ------
@@ -758,8 +765,13 @@ def clean_params(estimator):
 
     for name, p in estimator_params.items():
         # all potential unauthorized file write
-        if name.endswith('memory') or name.endswith('_path'):
+        if name == 'memory' or name.endswith('__memory') \
+                or name.endswith('_path'):
             new_p = {name: None}
+            estimator.set_params(**new_p)
+        elif n_jobs is not None and (name == 'n_jobs' or
+                                     name.endswith('__n_jobs')):
+            new_p = {name: n_jobs}
             estimator.set_params(**new_p)
         elif name.endswith('callbacks'):
             for cb in p:
@@ -767,4 +779,5 @@ def clean_params(estimator):
                 if cb_type not in ALLOWED_CALLBACKS:
                     raise ValueError(
                         "Prohibited callback type: %s!" % cb_type)
+
     return estimator
